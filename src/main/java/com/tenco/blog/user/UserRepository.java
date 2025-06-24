b53d40483326849ee0e8e15139cebc4a8dbfea97
sonjiyoon12 <1wldbs1908@gmail.com>
2025-06-24 11:16:09 +0900

    회원가입 확인 처리

---
+++ src/main/java/com/tenco/blog/user/UserRepository.java
@@ -0,0 +1,56 @@
+package com.tenco.blog.user;
+
+// 디비 접근 쿼리, 트랜잭션
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor // 생성자 의존 주입 - DI 처리
+@Repository // IoC 대상 + 싱글톰 패턴으로 관리 됨
+public class UserRepository {
+
+    // 생성자 의존 주입 - DI
+    private final EntityManager em;
+
+
+    /**
+     * 회원정보 저장 처리
+     *
+     * @param user (비영속 상태)
+     * @return user 엔티티 반환
+     */
+    @Transactional // select 제외하고 걸어주기
+    public User save(User user) {
+        // 매개 변수에 들어오는 user object 는 비영속화 된 상태이다.
+
+        em.persist(user); // 영속성 컨텍스트에 user 객체 관리하기 시작함
+
+        // 트랜잭션이 끝나면 커밋 시점에 실제 INSERT 쿼리를 실행한다.
+        return user;
+    }
+
+    // 사용자명 중복 체크용 조회 기능
+    public User findByUsername(String username) {
+
+//        // where username = ?
+//        // select --> 네이티브 쿼리, JPQL 중 선택 (JPQL 사용)
+//        String jpql = "SELECT u FROM User u WHERE u.username = :username ";
+//        TypedQuery<User> typedQuery = em.createQuery(jpql, User.class); // User.class 타입의 안정성
+//        typedQuery.setParameter("username", username);
+//        return typedQuery.getSingleResult();
+
+        try {
+            String jpql = "SELECT u FROM User u WHERE u.username = :username ";
+            return em.createQuery(jpql, User.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+
+}
